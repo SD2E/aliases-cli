183bb5fdfbaa30f9a81758cde1869f5781930c33
Greg Zynda <zyndagj@gmail.com>
2018-04-17 12:49:37 -0400

    Fixed tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,22 +1,38 @@
 """Tests for our main skele CLI module."""
 
 
-from subprocess import PIPE, Popen as popen
 from unittest import TestCase
+from StringIO import StringIO
 
+import sys
+from syd.cli import main
 from syd import __version__ as VERSION
 
+try:
+	from unittest.mock import patch
+except:
+	from mock import patch
 
 class TestHelp(TestCase):
     def test_returns_usage_information(self):
-        output = popen(['syd', '-h'], stdout=PIPE).communicate()[0]
-        self.assertTrue('Usage:' in str(output))
-
-        output = popen(['syd', '--help'], stdout=PIPE).communicate()[0]
-        self.assertTrue('Usage:' in str(output))
-
+	testArgs = ['None','-h']
+	with patch('sys.argv', testArgs):
+		with patch('sys.stdout', StringIO()):
+			with self.assertRaises(SystemExit):
+				main()
+        		self.assertTrue('Usage:' in sys.stdout.getvalue())
+	testArgs = ['None','--help']
+	with patch('sys.argv', testArgs):
+		with patch('sys.stdout', StringIO()):
+			with self.assertRaises(SystemExit):
+				main()
+			self.assertTrue('Usage:' in sys.stdout.getvalue())
 
 class TestVersion(TestCase):
     def test_returns_version_information(self):
-        output = popen(['syd', '--version'], stdout=PIPE).communicate()[0]
-        self.assertEqual(output.strip().decode('utf-8'), VERSION)
+	testArgs = ['None','--version']
+	with patch('sys.argv', testArgs):
+		with patch('sys.stdout', StringIO()):
+			with self.assertRaises(SystemExit):
+				main()
+				self.assertEqual(VERSION in sys.stdout.getvalue())
