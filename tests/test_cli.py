f3f2ad9dfd7482bc92f1dbb859910829a586d2f4
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2018-04-17 12:41:48 -0500

    fixed tests to work under 2.7/3.6 (tox)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,37 +2,41 @@
 
 
 from unittest import TestCase
-from StringIO import StringIO
 
 import sys
 from syd.cli import main
 from syd import __version__ as VERSION
 
 try:
-	from unittest.mock import patch
-except:
-	from mock import patch
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
 
 class TestHelp(TestCase):
     def test_returns_usage_information(self):
-	testArgs = ['None','-h']
-	with patch('sys.argv', testArgs):
-		with patch('sys.stdout', StringIO()):
-			with self.assertRaises(SystemExit):
-				main()
-        		self.assertTrue('Usage:' in sys.stdout.getvalue())
-	testArgs = ['None','--help']
-	with patch('sys.argv', testArgs):
-		with patch('sys.stdout', StringIO()):
-			with self.assertRaises(SystemExit):
-				main()
-			self.assertTrue('Usage:' in sys.stdout.getvalue())
+        testArgs = ['None', '-h']
+        with patch('sys.argv', testArgs):
+            with patch('sys.stdout', StringIO()):
+                with self.assertRaises(SystemExit):
+                    main()
+                self.assertTrue('Usage:' in sys.stdout.getvalue())
+        testArgs = ['None', '--help']
+        with patch('sys.argv', testArgs):
+            with patch('sys.stdout', StringIO()):
+                with self.assertRaises(SystemExit):
+                    main()
+                self.assertTrue('Usage:' in sys.stdout.getvalue())
 
 class TestVersion(TestCase):
     def test_returns_version_information(self):
-	testArgs = ['None','--version']
-	with patch('sys.argv', testArgs):
-		with patch('sys.stdout', StringIO()):
-			with self.assertRaises(SystemExit):
-				main()
-				self.assertEqual(VERSION in sys.stdout.getvalue())
+        testArgs = ['None', '--version']
+        with patch('sys.argv', testArgs):
+            with patch('sys.stdout', StringIO()):
+                with self.assertRaises(SystemExit):
+                    main()
+                self.assertEqual(VERSION, sys.stdout.getvalue().rstrip())
